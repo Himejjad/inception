cece7a291ba28101a11657b539ed41326fe3a580
himejjad <zaghimizoo@gmail.com>
2024-12-05 19:00:13 +0100

    ok

---
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 2
+
+mysql_secure_installation << EOF
+$root_pass
+n
+n
+Y
+Y
+Y
+EOF
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${data_name} ;"
+mariadb -e "CREATE USER IF NOT EXISTS '${data_user}'@'%' IDENTIFIED BY '${data_pwd}' ;"
+mariadb -e "GRANT ALL PRIVILEGES ON ${data_name}.* TO '${data_user}'@'%' ;"
+mariadb -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+old="bind-address            = 127.0.0.1"
+new="bind-address            = 0.0.0.0"
+sed -i "s/$old/$new/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysqld_safe
